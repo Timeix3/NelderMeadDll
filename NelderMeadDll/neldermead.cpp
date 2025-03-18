9e6343452e643b950583c72fda9fbdfb0af556a4
Timeix3 <timeix3@gmail.com>
2025-03-18 19:15:42 +0300

    Добавьте файлы проекта.

---
+++ NelderMeadDll/neldermead.cpp
@@ -0,0 +1,128 @@
+
+#include "pch.h"
+#include "neldermead.h"
+
+double evaluateFunction(vector<double> point, string function) {
+	te_parser tep;
+	set<te_variable> variables;
+	for (int i = 0; i < point.size(); i++)
+		variables.insert({ "x" + to_string(i + 1), &point[i] });
+	tep.set_variables_and_functions(variables);
+	double result = tep.evaluate(function);
+	try {
+		if (!tep.success()) throw runtime_error("Incorrect expression");
+	}
+	catch (const runtime_error& e) {
+		cerr << "Error: " << e.what() << endl;
+		exit(-1);
+	}
+	return result;
+}
+
+vector<element> makeStartSimplex(int varsCount, double scale, vector<double> startingPoint, string function) {
+	vector<element> elements;
+	elements.push_back(element(startingPoint, function));
+	for (int i = 0; i < varsCount; i++) {
+		vector<double> newPoint(varsCount, 0);
+		newPoint[i] = scale;
+		elements.push_back(element(newPoint, function));
+	}
+	return elements;
+}
+
+vector<double> operator*(const vector<double>& vec, double scalar) {
+	vector<double> result(vec.size());
+	for (int i = 0; i < vec.size(); i++) {
+		result[i] = vec[i] * scalar;
+	}
+	return result;
+}
+
+vector<double> operator/(const vector<double>& vec, double scalar) {
+	vector<double> result(vec.size());
+	for (int i = 0; i < vec.size(); i++) {
+		result[i] = vec[i] / scalar;
+	}
+	return result;
+}
+
+vector<double> operator+(const vector<double>& vec1, const vector<double>& vec2) {
+	vector<double> result(vec1.size());
+	for (int i = 0; i < vec1.size(); i++) {
+		result[i] = vec1[i] + vec2[i];
+	}
+	return result;
+}
+
+vector<double> operator-(const vector<double>& vec1, const vector<double>& vec2) {
+	vector<double> result(vec1.size());
+	for (int i = 0; i < vec1.size(); i++) {
+		result[i] = vec1[i] - vec2[i];
+	}
+	return result;
+}
+
+vector<double> calculateMassCenter(vector<element> elements) {
+	vector<double> massCenter(elements.size() - 1);
+	for (int i = 0; i < elements.size() - 1; i++)
+		massCenter = massCenter + elements[i].point / (double)(elements.size() - 1);
+	return massCenter;
+}
+
+bool compare(const element a, const element b)
+{
+	return a.functionValue < b.functionValue;
+}
+
+bool endCheck(double eps, vector<element> elements) {
+	double sum = 0;
+	for (int i = 1; i < elements.size(); i++)
+	{
+		sum += pow(elements[i].functionValue - elements.front().functionValue, 2);
+	}
+	return (sqrt(sum / (elements.size() - 1)) <= eps);
+}
+
+element calculateContraction(vector<element> elements, element reflection, vector<double> massCenter, double contractionCoeff, string function)
+{
+	element contraction;
+	if (elements.back().functionValue <= reflection.functionValue)
+		contraction = element(massCenter + (elements.back().point - massCenter) * contractionCoeff, function);
+	else contraction = element(massCenter + (reflection.point - massCenter) * contractionCoeff, function);
+	return contraction;
+}
+
+vector<double> findFunctionMinimum(int varsCount, vector<double> startingPoint, string function) {
+	double reflectionCoeff = 1;
+	double contractionCoeff = 0.5;
+	double expansionCoeff = 2;
+	double scale = 1;
+	double eps = 0.001;
+	vector<element> elements = makeStartSimplex(varsCount, scale, startingPoint, function);
+	int k = 0;
+	for (;;) {
+		sort(begin(elements), end(elements), compare);
+		if (endCheck(eps, elements) || k == 200) break;
+		k++;
+		vector<double> massCenter = calculateMassCenter(elements);
+		element reflection = element(massCenter * (1 + reflectionCoeff) - elements.back().point * reflectionCoeff, function);
+		if (elements.front().functionValue <= reflection.functionValue && reflection.functionValue <= elements.at(elements.size() - 2).functionValue) {
+			elements.back() = reflection;
+		}
+		else if (reflection.functionValue < elements.front().functionValue) {
+			element expansion = element(massCenter * (1 - expansionCoeff) + reflection.point * expansionCoeff, function);
+			if (expansion.functionValue < reflection.functionValue) elements.back() = expansion;
+			else elements.back() = reflection;
+		}
+		else {
+			element contraction = calculateContraction(elements, reflection, massCenter, contractionCoeff, function);
+			if (contraction.functionValue < min(elements.back().functionValue, reflection.functionValue))
+				elements.back() = contraction;
+			else {
+				for (int i = 1; i < elements.size(); i++)
+					elements[i] = element(elements[i].point + ((elements.front().point - elements[i].point) / 2.0), function);
+			}
+		}
+	}
+	return elements.front().point;
+}
---
+++ NelderMeadDll/neldermead.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#ifdef MYDLL_EXPORTS
+#define MYDLL_API __declspec(dllexport)
+#else
+#define MYDLL_API __declspec(dllimport)
+#endif
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include "tinyexpr.h"
+
+using namespace std;
+
+extern "C" MYDLL_API double evaluateFunction(vector<double> point, string function);
+
+class element {
+public:
+	vector<double> point;
+	double functionValue;
+	element() : point({ 0, 0 }), functionValue(0) {}
+	element(vector<double> p, string function) {
+		point = p;
+		functionValue = evaluateFunction(p, function);
+	}
+	void print() {
+		cout << "X=(";
+		for (int i = 0; i < point.size(); ++i) {
+			cout << point[i];
+			if (i < point.size() - 1) cout << ", ";
+		}
+		std::cout << ")" << " F(X)=" << functionValue << endl;
+	}
+};
+
+vector<element> makeStartSimplex(int varsCount, double scale, vector<double> startingPoint, string expression);
+vector<double> operator*(const vector<double>& vec, double scalar);
+vector<double> operator/(const vector<double>& vec, double scalar);
+vector<double> operator+(const vector<double>& vec1, const vector<double>& vec2);
+vector<double> operator-(const vector<double>& vec1, const vector<double>& vec2);
+vector<double> calculateMassCenter(vector<element> elements);
+bool compare(const element a, const element b);
+bool endCheck(double eps, vector<element> elements);
+element calculateContraction(vector<element> elements, element reflection, vector<double> massCenter, double contractionCoeff, string expression);
+extern "C" MYDLL_API vector<double> findFunctionMinimum(int varsCount, vector<double> startingPoint, string function);
